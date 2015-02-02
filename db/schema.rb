eec41307b280d61d679d7bcf4df9e604ad7feea3
MoncefB <moncef.biaz@polytechnique.edu>
2015-02-02 22:54:55 +0100

    DB + Style OK

---
+++ db/schema.rb
@@ -0,0 +1,33 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150202213511) do
+
+  create_table "users", force: true do |t|
+    t.integer  "users_id"
+    t.string   "users_name"
+    t.string   "users_pass"
+    t.string   "users_mail"
+    t.date     "users_birthdate"
+    t.string   "users_origin"
+    t.string   "users_description"
+    t.string   "users_avatar"
+    t.string   "users_links"
+    t.integer  "users_status"
+    t.string   "users_lon"
+    t.string   "users_lat"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create, user: { users_avatar: @user.users_avatar, users_birthdate: @user.users_birthdate, users_description: @user.users_description, users_id: @user.users_id, users_lat: @user.users_lat, users_links: @user.users_links, users_lon: @user.users_lon, users_mail: @user.users_mail, users_name: @user.users_name, users_origin: @user.users_origin, users_pass: @user.users_pass, users_status: @user.users_status }
+    end
+
+    assert_redirected_to user_path(assigns(:user))
+  end
+
+  test "should show user" do
+    get :show, id: @user
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @user
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch :update, id: @user, user: { users_avatar: @user.users_avatar, users_birthdate: @user.users_birthdate, users_description: @user.users_description, users_id: @user.users_id, users_lat: @user.users_lat, users_links: @user.users_links, users_lon: @user.users_lon, users_mail: @user.users_mail, users_name: @user.users_name, users_origin: @user.users_origin, users_pass: @user.users_pass, users_status: @user.users_status }
+    assert_redirected_to user_path(assigns(:user))
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user
+    end
+
+    assert_redirected_to users_path
+  end
+end
