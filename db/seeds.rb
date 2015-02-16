1abe16dd92738b7a08a7638cd8197d621a8d03ce
MoncefB <moncef.biaz@polytechnique.edu>
2015-02-16 23:45:17 +0100

    Before Adding Videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 User.create(
     users_name: "Moncef",
-    users_pass: "pass1",
+    users_pass: "pass",
     users_mail: "biaz@me.com",
     users_birthdate: "1991-09-24",
     users_origin: "Rabat",
@@ -22,7 +22,7 @@ User.create(
 
 User.create(
     users_name: "Yacine",
-    users_pass: "pass2",
+    users_pass: "pass",
     users_mail: "yacine.brini@polytechnique.edu",
     users_birthdate: "1991-12-05",
     users_origin: "Rabat",
@@ -33,3 +33,108 @@ User.create(
     users_lon: 2.203598,
     users_lat: 48.714593
  )
+
+User.create(
+    users_name: "Kenza",
+    users_pass: "pass",
+    users_mail: "kenzaidr@me.com",
+    users_birthdate: "1991-11-07",
+    users_origin: "Rabat",
+    users_description: "RnB",
+    users_avatar: "http://nsm08.casimages.com/img/2014/12/11//14121112180018601812785509.png",
+    users_links: "None",
+    users_status: 1,
+    users_lon: 2.289025,
+    users_lat: 48.765931 
+ )
+
+User.create(
+    users_name: "Greg",
+    users_pass: "pass",
+    users_mail: "gregoire.rigout@polytechnique.edu",
+    users_birthdate: "1991-12-05",
+    users_origin: "Paris",
+    users_description: "House",
+    users_avatar: "http://nsm08.casimages.com/img/2014/12/11//14121112180018601812785507.png",
+    users_links: "None",
+    users_status: 1,
+    users_lon: 2.163544,
+    users_lat: 48.707895 
+ )
+
+User.create(
+    users_name: "Ines",
+    users_pass: "pass",
+    users_mail: "ines@brini.com",
+    users_birthdate: "1994-08-12",
+    users_origin: "Rabat",
+    users_description: "Disney Fan",
+    users_avatar: "http://nsm08.casimages.com/img/2014/12/11//14121112180018601812785508.png",
+    users_links: "None",
+    users_status: 1,
+    users_lon: 2.288467,
+    users_lat: 48.762501
+ )
+
+User.create(
+    users_name: "Amine",
+    users_pass: "pass",
+    users_mail: "amine.mezzour@polytechnique.edu",
+    users_birthdate: "1991-01-22",
+    users_origin: "Meknes",
+    users_description: "Reggae",
+    users_avatar: "http://nsm08.casimages.com/img/2014/12/11//14121112180018601812785506.png",
+    users_links: "None",
+    users_status: 1,
+    users_lon: 2.217193,
+    users_lat: 48.713267
+ )
+
+User.create(
+    users_name: "Philou",
+    users_pass: "pass",
+    users_mail: "philippe.orhan@polytechnique.edu",
+    users_birthdate: "1992-08-07",
+    users_origin: "Paris",
+    users_description: "DeepHouse",
+    users_avatar: "http://nsm08.casimages.com/img/2014/12/11//14121112222018601812785515.png",
+    users_links: "None",
+    users_status: 1,
+    users_lon: 2.217380,
+    users_lat: 48.713600
+ )
+
+User.create(
+    users_name: "Mathou",
+    users_pass: "pass",
+    users_mail: "mathilde.fossat@polytechnique.edu",
+    users_birthdate: "1992-03-10",
+    users_origin: "Marseille",
+    users_description: "Latino",
+    users_avatar: "http://nsm08.casimages.com/img/2014/12/11//14121112222018601812785514.png",
+    users_links: "None",
+    users_status: 1,
+    users_lon: 2.217205,
+    users_lat: 48.713300
+ )
+
+User.create(
+    users_name: "Bonnav'",
+    users_pass: "pass",
+    users_mail: "noe.denys-de-bonnaventure@polytechnique.edu",
+    users_birthdate: "1993-10-25",
+    users_origin: "Bonnaventure",
+    users_description: "Zoumba",
+    users_avatar: "http://nsm08.casimages.com/img/2014/12/11//14121112222018601812785513.png",
+    users_links: "None",
+    users_status: 0,
+    users_lon: 2.217486,
+    users_lat: 48.713500
+ )
+
+
+
+
+
+
+
